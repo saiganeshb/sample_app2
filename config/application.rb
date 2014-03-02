434769bd0d000424c401e90c5d451e0b819c28d5
Sai Ganesh B <saiganeshb@gmail.com>
2014-03-01 22:35:06 -0800

    add to master

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module SampleApp2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     I18n.enforce_available_locales = true
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
